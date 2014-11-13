950588b4b468dfdebe5c1f329470f197e2dfe9ce
NumahoSouta <t14681sn@sfc.keio.ac.jp>
2014-11-13 11:42:24 +0900

    test

--- app/09/factorial.js
+++ app/09/factorial.js
@@ -1,1 +1,23 @@
+var factorial = function(a){
+  if(Number.isInteger(a) == false){
+    return null;
+  }
 
+  function factorial(num){
+    if(num == 1){
+      return 1;
+    }
+    if(num > 1){
+      return (num * factorial(num-1));
+    }
+
+  }
+
+
+
+
+
+
+
+
+}
--- app/10/prime.js
+++ app/10/prime.js
@@ -1,1 +1,12 @@
+var prime = function(a){
+  if(a == 1){
+    return false;
+  }
 
+
+  if(a > 0){
+    return true;
+  }
+
+
+}
